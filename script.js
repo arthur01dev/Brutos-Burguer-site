97fa27b496cc6353fca8a476e714e74312f6d636
Arthur Rodrigues de Souza <arthurrodriguesdesouza66@gmail.com>
2024-07-30 00:52:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+const menu = document.querySelector('#menu');
+const cartBtn = document.querySelector('#cart-btn');
+const cartModal = document.querySelector('#cart-modal');
+const cartItemsContainer = document.querySelector('#cart-items');
+const cartTotal = document.querySelector('#cart-total');
+const checkoutBtn = document.querySelector('#checkout-btn');
+const closeModalBtn = document.querySelector('#close-modal-btn');
+const cartCounter = document.querySelector('#cart-count');
+const addressInput = document.querySelector('#address');
+const addressWarn = document.querySelector('#address-warn');
+
+let cart = [];
+
+// Abrir o modal do carrinho
+cartBtn.addEventListener('click', () => {
+    updateCartModal();
+    cartModal.style.display = 'flex';
+});
+
+// Fechar o modal quando clicar fora dele
+cartModal.addEventListener('click', (event) => {
+    if (event.target === cartModal) {
+        cartModal.style.display = 'none'
+    }
+});
+
+closeModalBtn.addEventListener('click', () => {
+    cartModal.style.display = 'none'
+})
+
+menu.addEventListener('click', (event) => {
+    let parentButton = event.target.closest(".add-to-cart-btn");
+
+    if (parentButton) {
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+
+        addToCart(name, price)
+    }
+});
+
+// função para adicionar ao carrinho
+function addToCart(name, price) {
+
+    const existingItem = cart.find(item => item.name === name);
+
+    if (existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        cart.push({
+            name,
+            price,
+            quantity: 1
+        })
+    }
+
+    updateCartModal();
+}
+
+// Atualiza Carrinho
+function updateCartModal() {
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement('div');
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col");
+
+        cartItemElement.innerHTML = `
+        <div class="flex items-center justify-between border-b border-gray-500 border-solid">
+            <div>
+               <p class="font-medium">${item.name}</p>
+               <p>Qtd: ${item.quantity}</p>
+               <p claa="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+            </div>
+
+               <button>
+                   Remover
+               </button> 
+        </div>
+        `
+
+        total += item.price * item.quantity;
+
+        cartItemsContainer.appendChild(cartItemElement);
+    })
+
+    cartTotal.textContent = total.toLocaleString("pt-BR",{
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.textContent = cart.length;
+}
