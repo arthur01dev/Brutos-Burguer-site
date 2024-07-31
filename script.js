ee60d42cd946a6b9b9c2b251aeac36fe98d85ac6
Arthur Rodrigues de Souza <arthurrodriguesdesouza66@gmail.com>
2024-07-30 23:48:49 -0300

    Atualizando arquivo js e stylizando btn fechar

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function updateCartModal() {
                <p claa="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
             </div>
 
-               <button>
+               <button class="remove-from-cart-btn" data-name="${item.name}">
                    Remover
                </button> 
         </div>
@@ -91,4 +91,30 @@ function updateCartModal() {
     });
 
     cartCounter.textContent = cart.length;
+}
+
+// função para remover item do carrinho
+cartItemsContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("remove-from-cart-btn")) {
+        const name = event.target.getAttribute("data-name");
+
+        removeItemCart(name);
+    }
+})
+
+function removeItemCart(name) {
+    const index = cart.findIndex(item => item.name === name);
+
+    if (index !== -1) {
+        const item = cart[index];
+        
+        if(item.quantity > 1) {
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
+    }
 }
