d802f0a715610a3d8ab7f90bc08107b44604ef70
Arthur Rodrigues de Souza <arthurrodriguesdesouza66@gmail.com>
2024-08-03 20:55:53 -0300

    atualizando numero de wpp

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ checkoutBtn.addEventListener("click", () => {
     }).join("");
 
     const message = encodeURIComponent(cartItems);
-    const phone = "553496450498"
+    const phone = "5534998897373"
 
     window.open(`https://wa.me/${phone}?text=Boa noite Brutos bruguer🍔, meu pedido é: ${message}, Observação: ${inputObs}, Endereço: ${addressInput.value}`, "_blank");
 
