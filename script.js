a2d27260dd48a398541672ffeddbb998488039ee
Arthur Rodrigues de Souza <arthurrodriguesdesouza66@gmail.com>
2024-08-17 00:38:29 -0300

    mexendo novamente na mensagem

--- script.js
+++ script.js
@@ -206,13 +206,18 @@ checkoutBtn.addEventListener("click", () => {
         return;
     }
 
+    let total = 0;
+
     const cartItems = cart.map((item) => {
+        total += item.price * item.quantity;
         return (
-            ` ${item.name}, Quantidade: (${item.quantity}), Preço: R$${Number(item.price).toFixed(2)}. | `
+            ` ${item.name}, Quantidade: (${item.quantity}), Preço: R$${Number(item.price).toFixed(2)}. | %0A%0A`
         )
     }).join("");
 
-    const message = encodeURIComponent(cartItems);
+    const totalFormatted = `Total: ${total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}.%0A%0A`;
+
+    const message = encodeURIComponent(cartItems + totalFormatted);
     const phone = "5534998897373"
 
     window.open(`https://wa.me/${phone}?text=Boa%20noite%20Brutos%20Bruguer🍔,%0A%0AMeu%20pedido%20é:%0A${message}%0A%0AObservação:%0A${inputObs}%0A%0AEndereço:%0A${addressInput.value}`, "_blank")
